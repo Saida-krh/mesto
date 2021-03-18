7a8237dbfd1030404388fbff9ebe31eb2fe2d792
Saida Karakhanova <sayakrh@yandex.kz>
2021-03-19 01:56:15 +0600

    JsStart-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,41 @@
-const popup = document.querySelector( selectors: 'popup');
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const openPopupClassName = 'popup_opened';
+const closeButton = document.querySelector('.popup__close-button');
+const title = document.querySelector('.profile__title');
+const subtitle = document.querySelector('.profile__subtitle');
+const inputTitle = document.getElementById('title');
+const inputSubTitle = document.getElementById('subtitle');
+const saveButton = document.querySelector('.popup__button')
+
+function popupToggler(){
+    popup.classList.toggle(openPopupClassName) 
+}
+
+editButton.addEventListener('click', function(){
+    openPopup()
+});
+
+closeButton.addEventListener('click', function(){
+    popupToggler()
+});
+
+function openPopup(){
+    popupToggler()
+    inputTitle.value = title.textContent;
+    inputSubTitle.value = subtitle.textContent;
+}
+
+saveButton.addEventListener('click', function(){
+    savePopupData()
+});
+
+function savePopupData(){
+    popupToggler()
+    title.textContent = inputTitle.value;
+    subtitle.textContent = inputSubTitle.value;
+}
+
+
 console.log('Popup', popup)
+
