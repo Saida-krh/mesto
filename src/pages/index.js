336e5381d72154c2a648f99ae2cd8595d51ba917
Saida Karakhanova <sayakrh@yandex.kz>
2021-05-22 03:08:27 +0600

    Project Mesto 8 sprint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import {Card} from './components/Card.js';
-import {FormValidator} from './components/FormValidator.js'
-import {popupSelectors} from './data.js'
-import {Section} from './components/Section.js'
-import {initialCards} from './initial-cards.js'
-import {PopupWithForm} from './components/PopupWithForm.js'
-import {PopupWithImage} from './components/PopupWithImage.js'
-import {UserInfo} from './components/UserInfo.js'
+import {Card} from '../scripts/components/Card.js';
+import {FormValidator} from '../scripts/components/FormValidator.js'
+import {popupSelectors} from '../scripts/data.js'
+import {Section} from '../scripts/components/Section.js'
+import {initialCards} from '../scripts/initial-cards.js'
+import {PopupWithForm} from '../scripts/components/PopupWithForm.js'
+import {PopupWithImage} from '../scripts/components/PopupWithImage.js'
+import {UserInfo} from '../scripts/components/UserInfo.js'
 import '../pages/index.css'; 
 
 const formEditElement = document.querySelector('form[name="form-edit"]');
